9451f35835c5d86e24d5309b8d92bb615c862de4
Gabrieli <gabrieli@MacBook-Air-de-Gabrieli.local>
2020-11-27 14:32:33 -0300

    css do exercicio 8 alterado

--- semana2/exercicio8 /scripts/main.js
+++ semana2/exercicio8 /scripts/main.js
@@ -8,4 +8,29 @@ Programe uma função a ser executada quando o usuário clicar no botão,
 que faça o cálculo da idade do usuário, 
 e então exiba a idade em um texto na página HTML. */
 
-document.getElementById("botao");
+//primeira maneira de fazer
+
+/* document.querySelector('button').onclick = function () {
+     var data = document.getElementById('data').value;
+     var datasplited = data.split('-');
+     var hoje = new Date().getFullYear();
+     var idade = hoje - datasplited[0];
+     document.getElementById('idade').innerText = idade;
+ } */
+
+
+function calcularIdade (hoje, Data) {
+    var idade = hoje - Data;
+    return idade;
+}
+
+document.querySelector('button').onclick = function () {
+    var data = document.getElementById('data').value;
+    var datasplited = data.split('-');
+    var hoje = new Date().getFullYear();
+    var idade = calcularIdade (hoje, datasplited[0]);
+    document.getElementById('idade').innerText = idade;
+}
+
+
+
