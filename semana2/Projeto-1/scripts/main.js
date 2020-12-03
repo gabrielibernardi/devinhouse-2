e379ea67765839d39b3dffc109b0d006d6d9595a
Gabrieli <gabrieli@MacBook-Air-de-Gabrieli.local>
2020-12-02 22:07:29 -0300

    javaScript added

--- semana2/Projeto-1/scripts/main.js
+++ semana2/Projeto-1/scripts/main.js
@@ -1,11 +1,32 @@
-var btnAdd = document.getElementById("btnAdd");
+var btnAdd = document.getElementById("btnAdd")
 
 function addNaLista() {
-    var lista = document.getElementById("listaTarefa")
-    var novaTarefa = document.getElementById("inserirTarefas").value;
-    var optItem = document.createElement("option");
-    optItem.textContent = novaTarefa;
-    lista.appendChild(optItem)
+    var list = document.getElementById("listaTarefa")
+    var newTask = document.getElementById("inserirTarefas").value
+    document.getElementById("inserirTarefas").value = ""
+    
+    var taskItem = document.createElement("li")
+    taskItem.setAttribute("class", "tarefa")
+    
+    var taskInput = document.createElement("input")
+    taskInput.setAttribute("type", "checkbox")
+    taskInput.setAttribute("id", "checkbox-1")
+    taskItem.appendChild(taskInput);
+    
+    var taskText = document.createElement("span")
+    taskText.setAttribute("class", "texto")
+    taskText.setAttribute("id", "text-1")
+    taskText.textContent = newTask
+    taskItem.appendChild(taskText)
+    
+    
+    var taskButton = document.createElement("button")
+    taskButton.setAttribute("class", "deletar")
+    taskButton.setAttribute("id", "delete-1")
+    taskButton.textContent = "X"
+    taskItem.appendChild(taskButton)
+ 
+    list.appendChild(taskItem)
 }
 
-btnAdd.addEventListener("click", addNaLista)
+btnAdd.addEventListener("click", addNaLista);
