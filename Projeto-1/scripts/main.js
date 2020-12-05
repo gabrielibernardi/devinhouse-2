5c6ea653e5c03c6c2a0d1314b9959d786d2ea989
gabrielibernardi <74473908+gabrielibernardi@users.noreply.github.com>
2020-12-05 14:15:10 -0300

    todolist finalizada

--- Projeto-1/scripts/main.js
+++ Projeto-1/scripts/main.js
@@ -1,12 +1,21 @@
 var btnAdd = document.getElementById("btnAdd")
 var userTasks = []
 
+function saveLocalStorage(item) {
+    userTasks.push(item)
+    localStorage.setItem("userTasks", JSON.stringify(userTasks))
+}
 
-function criarElemento(objeto, taskNumber) {
+
+function addNaLista() {
+    var taskNumber = Number(document.getElementsByClassName("tarefa").length) //esse .length vai contar quantos itens tem com a classe tarefa
     var list = document.getElementById("listaTarefa") //pegar o que está na lista
+    var newTask = document.getElementById("inserirTarefas").value //pegar o valor da textarea
+    document.getElementById("inserirTarefas").value = "" //pegar o valorda text area depois que clicou no adicionar
+
     var taskItem = document.createElement("li") //criando um elemento li para injetar no html
     taskItem.setAttribute("class", "tarefa") //aqui eu passo as classes que usei no html
-    taskItem.setAttribute("id", "tarefa-" + taskNumber)
+    taskItem.setAttribute("id", "tarefa-" + taskNumber) 
     //aqui eu estou concatenando a id da li que eu tinha no html com 
     // a var tasknumber que eu criei acima para contar quando elementos tem na li.
     //lembrabdo que o appendChlid vai lá no final pq esse o li é o elemento pai
@@ -15,7 +24,6 @@ function criarElemento(objeto, taskNumber) {
     //elementos dentro dele.
     taskInput.setAttribute("type", "checkbox")
     taskInput.setAttribute("id", "checkbox-" + taskNumber)
-    taskInput.checked = objeto.marcado
     taskInput.addEventListener("click", function () { //essa função antes do appendChild é para fazer o lineCheck, eu criei ela antes.
         lineCheck("text-" + taskNumber)
     })
@@ -24,8 +32,7 @@ function criarElemento(objeto, taskNumber) {
     var taskText = document.createElement("span")
     taskText.setAttribute("class", "texto")
     taskText.setAttribute("id", "text-" + taskNumber)
-
-    taskText.textContent = objeto.texto
+    taskText.textContent = newTask
     taskItem.appendChild(taskText)
 
     var taskButton = document.createElement("button")
@@ -33,73 +40,21 @@ function criarElemento(objeto, taskNumber) {
     taskButton.setAttribute("id", "delete-" + taskNumber)
     taskButton.textContent = "X"
     taskButton.addEventListener("click", function () { //essa função antes do appendChild é para deletar, eu criei ela antes.
+        lineCheck("text-" + taskNumber)
         removeTask("tarefa-" + taskNumber)
     })
     taskItem.appendChild(taskButton)
-
     list.appendChild(taskItem)
-
-    if (objeto.marcado == true) {
-        lineCheck("text-" + taskNumber)
-    }
-
-}
-
-function addNaLista() {
-    var newTasktext = document.getElementById("inserirTarefas").value //pegar o valor da textarea
-    document.getElementById("inserirTarefas").value = "" //pegar o valorda text area depois que clicou no adicionar
-
-    var itensNaTela = document.getElementsByClassName("tarefa").length
-    var item = {
-        "texto": newTasktext,
-        "marcado": false
-    }
-    criarElemento(item, itensNaTela)
-
-    saveLocalStorage(newTasktext, false)
-}
-
-function saveLocalStorage(text, checked) {
-    var objTask = {
-        "texto": text,
-        "marcado": checked
-    }
-    userTasks.push(objTask)
-    localStorage.setItem("userTasks", JSON.stringify(userTasks))
-}
-
-function carregarTarefas() {
-    var tarefas = JSON.parse(localStorage.getItem("userTasks"))
-    if (tarefas) {
-        userTasks = tarefas
-        for (var i = 0; i < userTasks.length; i++) {
-            criarElemento(userTasks[i], i)
-        }
-    }
+    saveLocalStorage(newTask)
+    
 }
 
 function removeTask(taskId) {
-    var confirmarResposta = confirm("Você realmente deseja remover?")
-    if (confirmarResposta) {
-        var taskNumber = taskId.split("-")[1]
-        var tarefas = JSON.parse(localStorage.getItem("userTasks"))
-        tarefas.splice(taskNumber, 1)
-        console.log(tarefas)
-        localStorage.setItem("userTasks", JSON.stringify(tarefas))
-        document.getElementById(taskId).remove()
-    }
+    document.getElementById(taskId).remove()
 }
 
 function lineCheck(textId) {
-    var taskNumber = textId.split("-")[1]
-    var tarefas = JSON.parse(localStorage.getItem("userTasks"))
-    if (document.getElementById("checkbox-" + taskNumber).checked) {
-        tarefas[taskNumber].marcado = true
-    } else {
-        tarefas[taskNumber].marcado = false
-    }
-    localStorage.setItem("userTasks", JSON.stringify(tarefas))
-
+    console.log(textId)
     if (document.getElementById(textId).classList.contains("line")) {
         document.getElementById(textId).classList.remove("line")
     } else {
@@ -107,5 +62,27 @@ function lineCheck(textId) {
     }
 }
 
+var allTasks = document.getElementById("checkbox-0")
+for(var i = 0; i <= allTasks.length; i++){ 
+    console.log(allTasks)}
+    
+})
+    
+
+/*var allTasks = document.querySelectorAll(".tarefa")
+allTasks.forEach(
+    task => {
+        var taskId = task.getAttribute("id")
+        var taskNumber = taskId.split("-")[1]
+        var checkbox = document.getElementById("checkbox-" + taskNumber)
+        var button = document.getElementById("delete-" + taskNumber)
+        checkbox.addEventListener("click", function () {
+            lineCheck("text-" + taskNumber)
+        })
+        button.addEventListener("click", function () {
+            removeTask("tarefa-" + taskNumber)
+        })
+    }
+)*/
+
 btnAdd.addEventListener("click", addNaLista)
-carregarTarefas()
